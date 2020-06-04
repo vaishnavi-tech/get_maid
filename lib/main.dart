871a5f705ee6641dfcb3ab4506b0d3d67d08e5ff
preranashelke <preranashelke01@gmail.com>
2020-06-04 03:56:05 -0700

    5th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get_maid/categories_screen.dart';
 
@@ -12,7 +14,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'To find my maid ',
       theme: ThemeData(
-          primarySwatch: Colors.blue
+          primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          headline6: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          )
+        ),
       ),
       home: CategoriesScreen(),
     );
