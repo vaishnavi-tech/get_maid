2a799f894304acd217cdbed8e950d34fee561ac0
preranashelke <preranashelke01@gmail.com>
2020-06-03 21:17:36 -0700

    Initial commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import './model/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'babysitter',
+    color: Colors.purple,
+  ),
+  Category(
+      id: 'c2',
+      title: 'cooking',
+  color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'cleaning home',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c4',
+    title: 'elder care',
+    color: Colors.green,
+  ),Category(
+    id: 'c5',
+    title: 'driving',
+    color: Colors.orange,
+  ),Category(
+    id: 'c6',
+    title: 'washing clothes',
+    color: Colors.yellow,
+  ),
+
+];
+
