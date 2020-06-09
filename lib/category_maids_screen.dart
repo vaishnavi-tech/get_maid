bbab6b0fcf22fcd9926ce6697cc25cb4af02fbe2
preranashelke <preranashelke01@gmail.com>
2020-06-09 22:33:47 +0530

    modify  bottom tabBar

--- lib/category_maids_screen.dart
+++ lib/category_maids_screen.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 
 class CategoryMaidsScreen  extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-  CategoryMaidsScreen(this.categoryId, this.categoryTitle);
+  static const routeName = '/categories-maids';
+ // final String categoryId;
+  //final String categoryTitle;
+  //CategoryMaidsScreen(this.categoryId, this.categoryTitle);
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
          appBar: AppBar(
             title: Text(categoryTitle),
