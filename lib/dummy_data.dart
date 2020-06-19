eefd19eb74eefd6622304447e25abeeaa613f414
preranashelke <preranashelke01@gmail.com>
2020-06-19 14:00:43 +0530

    modify jobs

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,39 +1,63 @@
+
 import 'package:flutter/material.dart';
-//import './model/category.dart';
-import 'package:flutter/foundation.dart';
 
-class Maidlist{
-   final String id;
-  // ignore: non_constant_identifier_names
- final  String title;
-  // ignore: non_constant_identifier_names
- // String City;
-  // ignore: non_constant_identifier_names
-  //String State;
-  // ignore: non_constant_identifier_names
-  String person_involved;
-  // ignore: non_constant_identifier_names
-  //final double minimum_salary;
-  // ignore: non_constant_identifier_names
-   //final  double maximum_salary;
-
-
-  Maidlist({
-    @required this.id,
-    // ignore: non_constant_identifier_names
-    @required  this.title,
-    // ignore: non_constant_identifier_names
-    //this.City,
-    // ignore: non_constant_identifier_names
-    //this.State,
-    // ignore: non_constant_identifier_names
-    @required this.person_involved,
-    // ignore: non_constant_identifier_names
-   // @required this.minimum_salary,
-    // ignore: non_constant_identifier_names
-    //@required this.maximum_salary, color, FontWeight fontSize
-  } );
-
-
-
-}
+import './model/category.dart';
+const DUMMY_CATEGORIES = const[
+Category(
+  id: 'c1',
+title: 'Gardener',
+  color:  Colors.black12,
+  description: 'individual',
+),
+    Category(id: 'c2',
+title: 'Babysitter',
+color:  Colors.lightGreen,
+      description: 'individual',
+),
+Category(
+id: 'c3',
+title: 'Cook',
+color:  Colors.grey,
+  description: 'individual',
+),
+Category(id: 'c4',
+title: 'Home Cleaner',
+color:  Colors.transparent,
+  description: 'individual',
+),
+Category(id: 'c5',
+title: 'Elder Caretaker',
+color:  Colors.limeAccent,
+  description: 'individual',
+),
+Category(
+id: 'c6',
+title: 'Driver',
+color:  Colors.lightGreenAccent,
+  description: 'individual',
+),
+Category(
+id: 'c7',
+title: 'Laundry',
+color:  Colors.grey,
+  description: 'individual',
+),
+Category(
+id: 'c8',
+title: 'One Day Service',
+color:  Colors.black38,
+  description: 'individual',
+),
+Category(
+id: 'c9',
+title: 'Household Worker',
+color:  Colors.black54,
+  description: 'individual',
+),
+Category(
+id: 'c10',
+title: 'Nursing',
+color:  Colors.black26,
+  description: 'individual',
+),
+];
--- lib/model/category.dart
+++ lib/model/category.dart
@@ -4,11 +4,15 @@ class Category {
   final String id;
   final String title;
   final Color color;
+  final String description;
+
 
 
   const Category({
     @required this.id,
-     this.color= Colors.pink,
+    this.color,
+    @required this.description,
+
     @required this.title,
   });
 }
