787383a2320ee3777277e0ecd70005d895f63f7e
preranashelke <preranashelke01@gmail.com>
2020-06-19 14:02:30 +0530

    modify category item

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get_maid/model/category.dart';
 import '../screens/category_maids_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final List<Category> categories = [];
   final  String id;
   final String title;
   final Color color;
+  final String description;
 
-  CategoryItem(this.id , this.title ,this.color);
+  CategoryItem(this.id , this.title ,this.color, this.description);
   
   void selectCategory( BuildContext ctx){
     Navigator.of(ctx).pushNamed(
@@ -24,11 +28,11 @@ class CategoryItem extends StatelessWidget {
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
-        padding: const EdgeInsets.all(15),
+       padding: const EdgeInsets.all(15),
         child: Text(
           title,
           style: Theme.of(context).textTheme.headline6,
-        ),
+         ),
         decoration: BoxDecoration(
         gradient: LinearGradient(
         colors:[
