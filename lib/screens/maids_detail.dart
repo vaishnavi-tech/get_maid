129fd59eaae201eb496c679f5b45ff097e8d4a6e
preranashelke <preranashelke01@gmail.com>
2020-06-22 18:55:24 +0530

    navigation to maids detail page

---
+++ lib/screens/maids_detail.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MaidPage extends StatelessWidget {
+  final String title;
+  MaidPage( this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () {
+          print('back button pressed');
+        Navigator.pop(context ,false);
+        return Future.value(false);
+          },
+        child:Scaffold(
+           appBar: AppBar(
+           title: Text(title),
+       ),
+          body: Column(
+             crossAxisAlignment:  CrossAxisAlignment.center,
+            children: <Widget>[
+             Image.asset('assets/user.png'),
+             Container(
+              padding:EdgeInsets.all(10.0) ,
+              child: Text(title),),
+            Container(
+              padding:EdgeInsets.all(10.0),
+              color: Theme.of(context).accentColor,
+              child: RaisedButton(
+               child: Text('BACK'),
+               onPressed: () => Navigator.pop(context ),
+            ),),
+        ],
+      ),
+    ),
+    );
+  }
+}
