6c7540b54f99e73f152c31c8d6d5ebffa4ad43d6
preranashelke <preranashelke01@gmail.com>
2020-06-26 17:56:31 +0530

    added media Query

--- lib/screens/maids_detail.dart
+++ lib/screens/maids_detail.dart
@@ -2,10 +2,21 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/ui_elements/title_default.dart';
+import '../widgets/maids/address.dart';
 
 class MaidPage extends StatelessWidget {
   final String title;
-  MaidPage( this.title);
+ final String address;
+   final String gender;
+   final double age;
+   final double phonenumber;
+  MaidPage(
+      this.title,
+      this.address,
+      this.gender,
+      this.age,
+      this.phonenumber,
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +28,116 @@ class MaidPage extends StatelessWidget {
           },
         child:Scaffold(
            appBar: AppBar(
-           title: Text(title),
+             title: Text(title),
        ),
           body: Column(
              crossAxisAlignment:CrossAxisAlignment.center,
             children: <Widget>[
-             Image.asset('assets/user.png'),
-             Container(
-              padding:EdgeInsets.all(10.0) ,
-              child: TitleDefault(title)
+              Container(
+                padding:EdgeInsets.all(10.0) ,
+               child: Image.asset('assets/maid.png'),
+              ),
+            SizedBox(height: 20.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                padding:EdgeInsets.all(10.0) ,
+                 child:  Text(
+                     'NAME:',
+                   style: TextStyle(
+                       fontSize: 26.0,
+                       fontWeight: FontWeight.bold,
+                       fontFamily: 'Raleway'),
+                 ),
+              ),
+                Container(
+                 padding:EdgeInsets.all(10.0) ,
+                  child:  Address(title),
              ),
+              ],
+            ),
+              SizedBox(height: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding:EdgeInsets.all(10.0) ,
+                      child:  Text(
+                        'ADDRESS :',
+                          style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Raleway'),
+    ),
+                  ),
+                  Container(
+                    padding:EdgeInsets.all(10.0) ,
+                    child:
+                    TitleDefault(address),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+             Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding:EdgeInsets.all(10.0) ,
+                      child:  Text('GENDER:',
+                        style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Raleway'),
+                      ),
+                  ),
+                  Container(
+                    padding:EdgeInsets.all(10.0) ,
+                    child:Text(gender),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding:EdgeInsets.all(10.0) ,
+                      child:  Text('AGE:',
+                        style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Raleway'),
+                      ),
+                  ),
+                  Container(
+                    padding:EdgeInsets.all(10.0) ,
+                    child: Text(age.toString()),
+                    // child: ,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding:EdgeInsets.all(10.0) ,
+                      child:  Text('PHONE NUMBER:',
+                        style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Raleway'),
+                      ),
+                  ),
+                  Container(
+                    padding:EdgeInsets.all(10.0) ,
+                   child: Text(phonenumber.toString()),
+                   // child: ,
+                  ),
+                ],
+              ),
+
             Container(
               padding:EdgeInsets.all(10.0),
               color: Theme.of(context).accentColor,
